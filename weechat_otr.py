be9c0d3d8c5066004daec21c566de7ffb0a33aaa
Matthew M. Boedicker <matthewm@boedicker.org>
2010-12-01 22:45:47 -0500

    add Socialist Millionaire's Protocol

--- weechat_otr.py
+++ weechat_otr.py
@@ -56,7 +56,7 @@ class OtrOps:
         return opdata['account_namer'].name(account, protocol)
 
     def create_privkey(self, opdata=None, accountname=None, protocol=None):
-        opdata['informer'].inform('generating private key in %s' % KEY_FILE)
+        opdata['informer'].inform('Generating private key in %s' % KEY_FILE)
         otr.otrl_privkey_generate(USERSTATE, KEY_FILE, accountname, protocol)
 
     def display_otr_message(self, opdata=None, accountname=None, protocol=None,
@@ -68,23 +68,12 @@ class OtrOps:
     def gone_secure(self, opdata=None, context=None):
         if context and context.active_fingerprint:
             if context.active_fingerprint.trust == 'verified':
-                opdata['informer'].inform('verified OTR connection started')
+                opdata['informer'].inform('Verified OTR connection started')
             else:
-                opdata['informer'].inform('unverified OTR connection started')
-
-                contact_fingerprint = otr.otrl_privkey_hash_to_human(
-                    context.active_fingerprint.fingerprint)
-                opdata['informer'].inform("%s's fingerprint is %s" % (
-                        opdata['remote_user'], contact_fingerprint))
-
-                your_fingerprint = otr.otrl_privkey_fingerprint(
-                    USERSTATE, opdata['local_user'], PROTOCOL)
-                opdata['informer'].inform(
-                    'your fingerprint is %s' % your_fingerprint)
-
-                opdata['informer'].inform(
-                    "please verify %s's fingerprint through a secure channel" %
-                    opdata['remote_user'])
+                opdata['informer'].inform('Unverified OTR connection started')
+                otr_verify_instructions(opdata, context)
+        else:
+            opdata['informer'].inform('Invalid context')
 
     def inject_message(self, opdata=None, accountname=None, protocol=None,
                        recipient=None, message=None):
@@ -104,17 +93,21 @@ class OtrOps:
     def new_fingerprint(self, opdata=None, userstate=None, accountname=None,
                         protocol=None, username=None, fingerprint=None):
         human_fingerprint = otr.otrl_privkey_hash_to_human(fingerprint)
-        opdata['informer'].inform('new fingerprint %s' % human_fingerprint)
+        opdata['informer'].inform('New fingerprint %s' % human_fingerprint)
 
     def policy(self, opdata=None, context=None):
         return otr.OTRL_POLICY_DEFAULT
+
+    def still_secure(self, context=None, is_reply=None):
+        # TODO find out what this is for
+        opdata['informer'].inform('Still secure %s' % is_reply)
 
     def update_context_list(self, opdata=None):
         pass
 
     def write_fingerprints(self, opdata=None):
         opdata['informer'].inform(
-            'writing fingerprints to %s' % FINGERPRINT_FILE)
+            'Writing fingerprints to %s' % FINGERPRINT_FILE)
         otr.otrl_privkey_write_fingerprints(USERSTATE, FINGERPRINT_FILE)
 
 def irc_user(nick, server):
@@ -136,19 +129,18 @@ def otr_irc_in_privmsg(data, message_type, server_name, args):
         nick = weechat.info_get('irc_nick_from_host', hostmask)
         sender = irc_user(nick, server_name)
 
-        opdata = dict(
-            account_namer=AccountNamer(),
-            commander=Commander(),
-            informer=Informer(nick, server_name),
-            local_user=current_user(server_name),
-            remote_user=sender,
-            )
+        opdata = otr_opdata(nick, server_name)
 
         is_internal, decrypted_message, tlvs = otr.otrl_message_receiving(
             USERSTATE, (OPS, opdata), opdata['local_user'], PROTOCOL, sender,
             message)
 
-        # TODO: check tlvs
+        context = otr_context_find(sender, opdata['local_user'])
+
+        if context and context.smstate:
+            otr_check_tlvs(opdata, tlvs, context)
+        else:
+            opdata['informer'].inform('Invalid context')
 
         if not is_internal:
             result = '%s PRIVMSG %s :%s' % (hostmask, channel, decrypted_message)
@@ -171,22 +163,13 @@ def otr_irc_out_privmsg(data, message_type, server_name, args):
 
             recipient = irc_user(recipient_nick, server_name)
 
-            informer=Informer(recipient_nick, server_name)
-
-            opdata = dict(
-                account_namer=AccountNamer(),
-                commander=Commander(),
-                informer=informer,
-                local_user=sender,
-                remote_user=recipient,
-                )
+            opdata = otr_opdata(recipient_nick, server_name)
 
             message = otr.otrl_message_sending(
                 USERSTATE, (OPS, opdata), sender, PROTOCOL, recipient, message,
                 None)
 
-            context, added = otr.otrl_context_find(
-                USERSTATE, recipient, sender, PROTOCOL, 1)
+            context = otr_context_find(recipient, sender)
 
             otr.otrl_message_fragment_and_send(
                 (OPS, opdata), context, message, otr.OTRL_FRAGMENT_SEND_ALL)
@@ -197,24 +180,158 @@ def otr_irc_out_privmsg(data, message_type, server_name, args):
 
     return result
 
+def otr_command(data, buffer, args):
+    result = weechat.WEECHAT_RC_ERROR
+
+    arg_parts = args.split(None, 5)
+
+    if arg_parts[0] == 'trust':
+        otr_trust(*arg_parts[1:3])
+        result = weechat.WEECHAT_RC_OK
+    elif arg_parts[0] == 'smp':
+        action = arg_parts[1]
+        if action == 'respond':
+            nick, server, secret = arg_parts[2:5]
+            otr_smp_respond(nick, server, secret)
+            result = weechat.WEECHAT_RC_OK
+        elif action == 'ask':
+            nick, server, secret = arg_parts[2:5]
+            if len(arg_parts) > 5:
+                question = arg_parts[5]
+            else:
+                question = None
+            otr_smp_ask(nick, server, secret, question)
+            result = weechat.WEECHAT_RC_OK
+
+    return result
+
+def otr_context_find(remote_user, local_user, add=1):
+    return otr.otrl_context_find(
+        USERSTATE, remote_user, local_user, PROTOCOL, add)[0]
+
+def otr_check_tlvs(opdata, tlvs, context):
+    if otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP_ABORT):
+        opdata['informer'].inform('SMP was aborted')
+    else:
+        smp1q_tlv = otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP1Q)
+
+        if context.smstate.sm_prog_state == otr.OTRL_SMP_PROG_CHEATED:
+            otr_smp_abort(opdata, context, 'cheated')
+
+        elif otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP1):
+            if otr_expect_smstate(opdata, context, otr.OTRL_SMP_EXPECT1):
+                opdata['informer'].inform(
+                    '%(remote_user)s has requested SMP verification' % opdata)
+                opdata['informer'].inform(
+                    'Respond with /otr smp respond %(remote_user)s %(server)s <secret>' % opdata)
+
+        elif smp1q_tlv:
+            if otr_expect_smstate(opdata, context, otr.OTRL_SMP_EXPECT1):
+                opdata['informer'].inform(
+                    '%s has requested SMP verification: %s' % (
+                        opdata['remote_user'], smp1q_tlv.data))
+                opdata['informer'].inform(
+                    'Respond with /otr smp respond %(remote_user)s %(server)s <answer>' % opdata)
+
+        elif otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP2):
+            if otr_expect_smstate(opdata, context, otr.OTRL_SMP_EXPECT2):
+                context.smstate.nextExpected = otr.OTRL_SMP_EXPECT4
+                opdata['informer'].inform('SMP progressing')
+
+        elif otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP3):
+            if otr_expect_smstate(opdata, context, otr.OTRL_SMP_EXPECT3):
+                context.smstate.nextExpected = otr.OTRL_SMP_EXPECT1
+                otr_smp_inform(opdata, context)
+
+        elif otr.otrl_tlv_find(tlvs, otr.OTRL_TLV_SMP4):
+            if otr_expect_smstate(opdata, context, otr.OTRL_SMP_EXPECT4):
+                context.smstate.nextExpected = otr.OTRL_SMP_EXPECT1
+                otr_smp_inform(opdata, context)
+
+def otr_expect_smstate(opdata, context, state):
+    if state == context.smstate.nextExpected:
+        return True
+    else:
+        otr_smp_abort(opdata, context, 'expecting smstate %s not %s' % (
+                context.smstate.nextExpected, state))
+        return False
+
+def otr_opdata(remote_user, server):
+    return dict(
+        account_namer=AccountNamer(),
+        commander=Commander(),
+        informer=Informer(remote_user, server),
+        local_user=current_user(server),
+        remote_user=remote_user,
+        server=server,
+        )
+
+def otr_smp_abort(opdata, context, detail=None):
+    message = 'Aborting SMP'
+    if detail:
+        message += ', %s' % detail
+    opdata['informer'].inform(message)
+    otr.otrl_message_abort_smp(USERSTATE, (OPS, opdata), context)
+
+def otr_smp_ask(nick, server, secret, question=None):
+    context = otr_context_find(irc_user(nick, server), current_user(server), 1)
+
+    opdata = otr_opdata(nick, server)
+
+    if question:
+        otr.otrl_message_initiate_smp_q(
+            USERSTATE, (OPS, opdata), context, question, secret)
+    else:
+        otr.otrl_message_initiate_smp(USERSTATE, (OPS, opdata), context, secret)
+
+def otr_smp_inform(opdata, context):
+    if context.smstate.sm_prog_state == otr.OTRL_SMP_PROG_SUCCEEDED:
+        message = 'SMP verification succeeded'
+    else:
+        message = 'SMP verification failed'
+
+    opdata['informer'].inform(message)
+
+def otr_smp_respond(nick, server, secret):
+    context = otr_context_find(irc_user(nick, server), current_user(server), 1)
+
+    otr.otrl_message_respond_smp(
+        USERSTATE, (OPS, otr_opdata(nick, server)), context, secret)
+
 def otr_trust(nick, server):
-    context = otr.otrl_context_find(
-        USERSTATE, irc_user(nick, server), current_user(server), PROTOCOL, 0)[0]
+    context = otr_context_find(irc_user(nick, server), current_user(server), 0)
 
     if context and context.active_fingerprint:
 	context.active_fingerprint.trust = 'verified'
 	otr.otrl_privkey_write_fingerprints(USERSTATE, FINGERPRINT_FILE)
 
-def otr_command(data, buffer, args):
-    result = weechat.WEECHAT_RC_ERROR
+def otr_verify_instructions(opdata=None, context=None):
+    d = dict(
+        remote_fingerprint=otr.otrl_privkey_hash_to_human(
+            context.active_fingerprint.fingerprint),
+        remote_user=opdata['remote_user'],
+        server=opdata['server'],
+        your_fingerprint=otr.otrl_privkey_fingerprint(
+            USERSTATE, opdata['local_user'], PROTOCOL))
 
-    arg_parts = args.split()
+    instructions = """
+You can verify that this contact is who they claim to be in one of 3 ways:
 
-    if arg_parts[0] == 'trust':
-        otr_trust(*arg_parts[1:3])
-        result = weechat.WEECHAT_RC_OK
+1) Verify that these fingerprints match using a secure channel:
+  Your fingerprint : %(your_fingerprint)s
+  %(remote_user)s's fingerprint : %(remote_fingerprint)s
 
-    return result
+  then use the command: /otr trust %(remote_user)s %(server)s
+
+2) SMP shared secret:
+  /otr smp ask %(remote_user)s %(server)s <secret>
+
+3) SMP shared secret with question:
+  /otr smp ask %(remote_user)s %(server)s <secret> <question>
+""" % d
+
+    for line in instructions.split("\n"):
+        opdata['informer'].inform(line)
 
 # otr setup
 USERSTATE = otr.otrl_userstate_create()
@@ -242,7 +359,9 @@ weechat.hook_modifier('irc_out_privmsg', 'otr_irc_out_privmsg', '')
 
 weechat.hook_command(
     'otr', 'Off-the-Record',
-    '[trust nick server]',
+    '[trust nick server] | '
+    '[smp respond nick server secret] | '
+    '[smp ask nick server secret [question]]',
     '',
     '',
     'otr_command',
