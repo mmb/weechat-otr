446abeb832e4b773fdd7a133f12bff5c674f666f
Matthew M. Boedicker <matthewm@boedicker.org>
2014-01-28 22:38:24 -0800

    Test key generation with non-ascii nicks.

--- weechat_otr_test/test_key_generation.py
+++ weechat_otr_test/test_key_generation.py
@@ -14,12 +14,27 @@ import weechat_otr
 class KeyGenerationTestCase(WeechatOtrTestCase):
 
     def test_creates_key_file(self):
+        sys.modules['weechat'].set_server_current_nick('server', 'noob')
+
         weechat_otr.message_in_cb(None, None, 'server',
-            ':nick!user@host PRIVMSG nick :?OTRv2?')
+            ':nick!user@host PRIVMSG noob :?OTRv2?')
 
         key_path = os.path.join(
             sys.modules['weechat'].weechat_dir,
             'otr',
-            'nick@server.key3')
+            'noob@server.key3')
+
+        self.assertGreater(os.path.getsize(key_path), 0)
+
+    def test_creates_key_file_non_ascii(self):
+        sys.modules['weechat'].set_server_current_nick('server', 'gefährte')
+
+        weechat_otr.message_in_cb(None, None, 'server',
+            ':nick!user@host PRIVMSG gefährte :?OTRv2?')
+
+        key_path = os.path.join(
+            sys.modules['weechat'].weechat_dir,
+            'otr',
+            'gefährte@server.key3')
 
         self.assertGreater(os.path.getsize(key_path), 0)
