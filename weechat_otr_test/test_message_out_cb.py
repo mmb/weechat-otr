8433c7e00420fa8d459ff34caf77bf4ca024b915
Matthew M. Boedicker <matthewm@boedicker.org>
2014-04-03 14:13:03 -0700

    Test for outbound messages to nicks starting with @.
    To prevent regressions like #67.

---
+++ weechat_otr_test/test_message_out_cb.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-public-methods
+
+from __future__ import unicode_literals
+
+from weechat_otr_test.weechat_otr_test_case import WeechatOtrTestCase
+
+import weechat_otr
+
+class WeechatOtrGeneralTestCase(WeechatOtrTestCase):
+
+    def test_message_out_cb_nick_with_at(self):
+        result = weechat_otr.message_out_cb(None, None, 'server',
+            ':nick!user@host PRIVMSG @#chan :hello')
+        self.assertEqual(result, 'PRIVMSG @#chan :hello')
