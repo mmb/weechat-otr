88f045c2f7dd48d78c713b4e2ac3ba1da38fb701
Matthew M. Boedicker <matthewm@boedicker.org>
2014-04-11 23:54:42 -0700

    Update script version to 1.4.0.

--- weechat_otr.py
+++ weechat_otr.py
@@ -149,7 +149,7 @@ To end your private conversation: /otr finish
 
 SCRIPT_AUTHOR = 'Matthew M. Boedicker'
 SCRIPT_LICENCE = 'GPL3'
-SCRIPT_VERSION = '1.3.0'
+SCRIPT_VERSION = '1.4.0'
 
 OTR_DIR_NAME = 'otr'
 
