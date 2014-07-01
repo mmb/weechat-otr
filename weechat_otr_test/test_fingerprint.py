84dc5854a46ba46e21416d95e6c7f8b87be644c5
Matthew M. Boedicker <matthewm@boedicker.org>
2014-06-30 22:32:42 -0700

    Start of tests for fingerprint command.

---
+++ weechat_otr_test/test_fingerprint.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-public-methods
+
+from __future__ import unicode_literals
+
+from weechat_otr_test.weechat_otr_test_case import WeechatOtrTestCase
+
+import weechat_otr
+
+class FingerprintTestCase(WeechatOtrTestCase):
+
+    def xtest_fingerprint(self):
+        account1 = weechat_otr.ACCOUNTS['nick@server']
+        account2 = weechat_otr.ACCOUNTS['nick2@server2']
+
+        weechat_otr.command_cb(None, None, 'fingerprint')
+
+        self.assertPrinted('', (
+            'eval(${{color:default}}:! ${{color:brown}}otr${{color:default}}'
+            ' !:)\t(color default)nick@server fingerprint '
+            '{fingerprint}'.format(fingerprint=account1.getPrivkey())))
+
+        self.assertPrinted('', (
+            'eval(${{color:default}}:! ${{color:brown}}otr${{color:default}}'
+            ' !:)\t(color default)nick2@server2 fingerprint '
+            '{fingerprint}'.format(fingerprint=account2.getPrivkey())))
+
+    def xtest_fingerprint_pattern(self):
+        pass
+
+    def xtest_fingerprint_all(self):
+        pass
